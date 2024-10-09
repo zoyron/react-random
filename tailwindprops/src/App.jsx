83fb5c3a9f3e08f665e1b2d184c9dce5f56b8e1f
zoyron <sgrarora3@gmail.com>
2024-10-10 00:21:08 +0530

    added the card component

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -1,12 +1,13 @@
-function App() {
-
+import Card from "./Card";
+const App = () => {
   return (
-    <>
-      <div>
-        <h1>Hello world! Let's do tailwind</h1>
-      </div>
-    </>
-  )
-}
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="bg-green-300 p-4 m-4 rounded-md text-center">
+        Hello world! Let's do tailwind
+      </h1>
+      <Card />
+    </div>
+  );
+};
 
-export default App
+export default App;
