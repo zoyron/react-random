ba5e3c8684a1f4f7bac27f06f254b78c45de76b3
zoyron <sgrarora3@gmail.com>
2024-10-11 18:54:20 +0530

    added three colors

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("white");
   return (
     <>
-      <h1>background changer</h1>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 bg-slate-600 px-3 py-2 rounded-xl">
+            <button
+              className="outline-none text-white p-2 rounded-md"
+              style={{ backgroundColor: "#fa3000" }}
+              onClick={() => setColor("#fa3000")}
+            >
+              Red
+            </button>
+            <button
+              className="outline-none text-white p-2 rounded-md"
+              style={{ backgroundColor: "#40fa00" }}
+              onClick={() => setColor("#40fa00")}
+            >
+              Green
+            </button>
+            <button
+              className="outline-none text-white p-2 rounded-md"
+              style={{ backgroundColor: "#0080fa" }}
+              onClick={() => setColor("#0080fa")}
+            >
+              Blue
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
